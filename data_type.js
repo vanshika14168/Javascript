d2857cdd1730a5e2fe937c0cd372555e13130d38
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-13 00:06:31 +0530

    Data type

---
+++ data_type.js
@@ -0,0 +1,31 @@
+//premitive data type
+//number
+let a =10
+let b =9.90
+console.log(a)
+console.log(a,b)
+console.log(typeof(a))
+console.log(typeof(b))
+
+//string
+let c="hello world"
+console.log(c)
+
+//boolean
+let login = true
+console.log(login)
+
+//undefined
+let user;
+console.log(user)
+console.log(typeof(user))
+
+//bigint
+let A=36563566826378682371986378n
+console.log(A)
+console.log(typeof(A))
+
+//null
+let weather=null
+console.log(weather)
+console.log(typeof(weather))
