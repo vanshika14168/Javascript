1eeb08bc7f3da7c16bdab2e239772c9207e805aa
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-13 00:07:38 +0530

    comparison operator

---
+++ comparison_operator.js
@@ -0,0 +1,11 @@
+console.log(45>89)
+console.log(8==8)
+console.log(45<900)
+console.log(2!=0)
+console.log(45>=33)
+console.log(90<=99)
+let a=90
+let b=99
+console.log(a==b)
+console.log(a>b)
+console.log(a<b)
