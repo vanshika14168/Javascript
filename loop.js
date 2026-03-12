865130ce0fb6216eff6de185332df30e8a686c34
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-13 00:08:32 +0530

    Loops

---
+++ loop.js
@@ -0,0 +1,22 @@
+//FOR LOOP
+console.log("for loop")
+for(let i=0; i<10; i++){
+    console.log(i)
+}
+
+//WHILE LOOP
+console.log("whileloop")
+let i=0
+while(i<10){
+    console.log(i)
+    i++
+}
+
+//DO while loop
+console.log("do while loop")
+let j=0
+do{
+    console.log(j)
+    j++
+}
+while(j<10)
