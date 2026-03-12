7b504ff0ec8e3aec3657e10b1d1a5d50c955800b
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-13 00:07:10 +0530

    Arithmetic operator

---
+++ Arithmetic_operator.js
@@ -0,0 +1,12 @@
+//Arithemetic operator
+let x=90
+let y =898
+console.log(x+y)
+x=x+y
+console.log(x)
+console.log(78+99)
+console.log(454-67)
+console.log(45*2)
+console.log(10/2)
+console.log(10%5)
+console.log(2**3)
