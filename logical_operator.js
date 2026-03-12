a8e9cfe8bc1c3f03a9b14d7cf0fe1ea2450da536
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-13 00:08:05 +0530

    Logical operator

---
+++ logical_operator.js
@@ -0,0 +1,26 @@
+//AND operator
+let x=90
+if(x>=80 && x==90){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+
+//OR operator
+if(x!=90 || x>100){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+
+//NOT EQUAL to
+if(x!=99){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
