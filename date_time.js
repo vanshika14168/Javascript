447bc10565ef6bfcbcc99369d653970ef7aef457
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-14 22:16:05 +0530

    date_time

---
+++ date_time.js
@@ -0,0 +1,8 @@
+//date
+const now=new Date()
+console.log(now)//it show universal (UTC) time
+
+console.log(now.toString())//it shows (IST) india standard time
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getMonth())
